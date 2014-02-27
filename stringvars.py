c37aae8248ae8efe16c56e652eef4aabcd519152
P Campbell <triciamontreal@gmail.com>
2014-02-27 17:17:29 -0500

    Create stringvars.py

---
+++ stringvars.py
@@ -0,0 +1,19 @@
+# now I am using variables 
+# variables are named containers 
+
+# they can be any name, I chose msg
+# here I put a string into a variable
+msg = "Hello World"
+
+
+# display a variable ?  
+# nope the " " mean it is a string m s g 
+print("msg")
+
+# display a variable ! 
+print(msg)
+
+
+# change the variable content
+msg = "Bonjour Monde"
+print(msg)

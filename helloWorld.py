f804d78a38228b1857d1055f86a483eea2d6714b
P Campbell <triciamontreal@gmail.com>
2014-02-27 16:14:28 -0500

    Create helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,14 @@
+# python is an interpreted language
+
+# This is a comment it begins with #
+# comments are ignored by the interpreter
+# you can put what you want here
+
+
+# blank lines are ok, they are called whitespace
+# they make it easier for you to read your code
+# blank lines are ignored by the interpreter
+
+# display hello world on the screen
+
+print("hello world")

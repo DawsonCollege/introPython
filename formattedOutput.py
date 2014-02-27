33164aa2bba947848863d7ba068711cf07c52f48
P Campbell <triciamontreal@gmail.com>
2014-02-27 17:00:16 -0500

    Create formattedOutput.py

---
+++ formattedOutput.py
@@ -0,0 +1,10 @@
+# print() is a function
+# it takes strings and formatting characters
+# then displays them on the screen
+
+# I can format the text a little 
+# the \n tells print to go to  a new line
+print("hello world \n from Monty!")
+
+# I can chain strings together 
+print ("hello " + " world " + " how are you ?")
